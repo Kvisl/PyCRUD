a89909c0ce1bc61204da737ae81eb67d0e31a0be
SVG <tanngniostr@yahoo.com>
2024-04-11 03:26:27 +0300

    change

--- clients.py
+++ clients.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2.sql import SQL, Identifier
 
 
 def create_db(conn):
@@ -14,8 +15,9 @@ def create_db(conn):
 
         cur.execute('''
         CREATE TABLE IF NOT EXISTS Phone_numbers(
+            id SERIAL PRIMARY KEY,
             number VARCHAR(20) NOT NULL UNIQUE,
-            id_client INT NOT NULL REFERENCES Clients(id)
+            id_client INT NOT NULL REFERENCES Clients(id) 
         );
         ''')
     conn.commit()
@@ -43,26 +45,26 @@ def add_phone_number(conn, id_client, number):
         return insert_phone
 
 
-def change_client(conn, id_client, first_name=None, last_name=None, email=None, number=None):
+def change_client(conn, client_id, first_name=None, last_name=None, email=None):
+    client = {'first_name': first_name, "last_name": last_name, 'email': email}
     with conn.cursor() as cur:
-        cur.execute('''
-        UPDATE Clients
-            SET first_name=%s, last_name=%s, email=%s
-            WHERE id=%s
-            RETURNING id, first_name, last_name, email;
-        ''', (first_name, last_name, email, id_client,))
-        update_client = cur.fetchone()
-        return update_client
+        for key, v in client.items():
+            if v:
+                cur.execute(
+                    SQL("UPDATE Clients SET {}=%s WHERE id=%s").format(Identifier(key)),
+                    (v, client_id)
+                )
+    return True
 
 
-def change_phone(conn, id_client, number):
+def change_phone(conn, id_client, number, id):
     with conn.cursor() as cur:
         cur.execute('''
         UPDATE phone_numbers
             SET  number=%s
-            WHERE id_client=%s
-            RETURNING id_client, number;
-        ''', (number, id_client,))
+            WHERE id_client=%s AND id=%s
+            RETURNING id_client, number, id;
+        ''', (number, id_client, id))
         update_phone = cur.fetchone()
         return update_phone
 
@@ -71,15 +73,20 @@ def delete_phone(conn, id_client, number):
     with conn.cursor() as cur:
         cur.execute('''
         DELETE FROM Phone_numbers
-            WHERE id_client=%s
+            WHERE id_client=%s AND number=%s
             RETURNING id_client, number;
-        ''', (id_client,))
+        ''', (id_client, number))
         delete = cur.fetchone()
         return delete
 
 
 def delete_client(conn, id):
     with conn.cursor() as cur:
+        cur.execute('''
+        DELETE FROM Phone_numbers
+            WHERE id_client=%s
+            RETURNING id_client, number;
+        ''', (id,))
         cur.execute('''
         DELETE FROM Clients
             WHERE id=%s
@@ -89,24 +96,27 @@ def delete_client(conn, id):
         return delete
 
 
-def find_client(conn, first_name=None, last_name=None, email=None, number=None):
+def find_client(conn, first_name='%', last_name='%', email='%', number='%'):
     with conn.cursor() as cur:
         cur.execute('''
-        SELECT Clients.first_name, Clients.last_name, Clients.email
-           FROM Clients
-           LEFT JOIN Phone_numbers ON Clients.id = Phone_numbers.id_client
-           WHERE first_name=%s OR last_name=%s OR email=%s OR number=%s;
-        ''', (first_name, last_name, email, number,))
-        select_client = cur.fetchone()
+        SELECT *
+        FROM Clients cl
+        LEFT JOIN Phone_numbers ph ON cl.id = ph.id_client
+        WHERE first_name LIKE %(first_name)s
+          AND last_name LIKE %(last_name)s
+          AND email LIKE %(email)s
+          AND number LIKE %(number)s;
+        ''', {'first_name': first_name, 'last_name': last_name, 'email': email, 'number': number})
+        select_client = cur.fetchall()
         return select_client
 
 
-with psycopg2.connect(database='clients', user='postgres', password='', host='localhost', port='5432') as conn:
+with psycopg2.connect(database='clients', user='postgres', password='530496', host='localhost', port='5432') as conn:
     create_db(conn)
     print(add_client(conn, 'Ivan', 'Petrov', 'ivan@mail.ru'))
     print(add_phone_number(conn, 1, '+78881234567'))
     print(change_client(conn, 1, 'Pavel', 'Ivanov', 'pavel@gmail.com'))
-    print(change_phone(conn, 1, '+75550987654'))
+    print(change_phone(conn, 1, '+75550987654', 1))
     print(delete_phone(conn, 1, '+77750987654'))
     print(delete_client(conn, 1))
-    print(find_client(conn, first_name='', last_name='',  email='ivan@mail.ru', number=''))
+    print(find_client(conn, first_name=''))
